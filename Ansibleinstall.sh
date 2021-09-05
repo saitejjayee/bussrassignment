fe9f311b912c9319519e9028cdd784fa60d28424
saitejjayee <jayee.saitej@gmail.com>
2021-09-05 23:28:52 +0530

    Add files via upload

---
+++ Ansibleinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+  sudo apt-get update
+  sudo apt-get install -y software-properties-common
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  sudo apt-get install -y ansible
+  echo "`ansible --version | head -1` installed Successfully"
+  
